98ba589fdab4486a2d8633b641c0d07a48022a28
Dik Medvescek Murovec <dik.med.mur@gmail.com>
2019-03-21 17:22:04 +0100

    getSiteContent reads and respects robots.txt

--- HTTPDowloader.py
+++ HTTPDowloader.py
@@ -1,22 +1,32 @@
 from bs4 import BeautifulSoup
 import aiohttp
 import asyncio
-import urllib
+from urllib.parse import urlparse
+import urllib.robotparser
 
+rp = urllib.robotparser.RobotFileParser()
 URL = 'http://e-uprava.gov.si'
+URL2 = 'http://e-uprava.gov.si/e-uprava/oglasnadeska.html'
+
 
 def __init__(self, URL):
     self.URL = URL
 
+
 async def getSiteContent(url):
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            text = await resp.read()
+        parsedUrl = urlparse(url)
+        rp.set_url(parsedUrl.scheme + "://" + parsedUrl.netloc + "/robots.txt")
+        rp.read()
+        if rp.can_fetch("*", url):
+            async with session.get(url) as resp:
+                text = await resp.read()
+        else:
+            return ""
 
-    print(BeautifulSoup(text.decode('utf-8'), 'html5lib'))
+    # print(BeautifulSoup(text.decode('utf-8'), 'html5lib'))
     return BeautifulSoup(text.decode('utf-8'), 'html5lib')
 
-async def robotsAllow(url):
-    return False
 
 asyncio.run(getSiteContent(URL))
+asyncio.run(getSiteContent(URL2))
