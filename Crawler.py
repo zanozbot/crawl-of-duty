771b8e0c047e8807d7b0bbff5c96301edc1136eb
Žan Ožbot <zan.ozbot@gmail.com>
2019-03-22 15:11:45 +0100

    Added URL normalization

--- Crawler.py
+++ Crawler.py
@@ -1,9 +1,17 @@
 from database.models import *
-
+from urllib.parse import urlparse
+import re
 
 class Crawler:
 
-  def __init__(self):
-      print(DataType(code="PDF").code)
+    def __init__(self):
+        print(DataType(code="PDF").code)
+        print(self.normalize_url('https://www.cwi.nl:80/%7Eguido/Python/1239/index.html?id=213'))
+
+    def normalize_url(self, url):
+        parsed_url = urlparse(url)
+        exploded_path = re.search("(.*\/)(.+)(\..+)", parsed_url.path)
+        return parsed_url.scheme + "://" + parsed_url.hostname + exploded_path[1] + exploded_path[2]
+
 
 Crawler()
