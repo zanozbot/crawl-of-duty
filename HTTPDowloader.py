872aca1d10ae449c53adcc757101e2fb6386bea3
Romanium <romankomac@gmail.com>
2019-03-24 15:44:27 +0100

    Added sitemap parser

--- HTTPDowloader.py
+++ HTTPDowloader.py
@@ -1,16 +1,20 @@
 from bs4 import BeautifulSoup
 import aiohttp
 from urllib.parse import urlparse
+from urllib.request import urlopen
 import urllib.robotparser
 from enum import Enum
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+import sys, re
+import xml.etree.ElementTree as ET
 
 rp = urllib.robotparser.RobotFileParser()
 URL = 'http://e-uprava.gov.si'
 URL2 = 'http://e-uprava.gov.si/e-uprava/oglasnadeska.html'
 URL3 = 'https://angular.io'
 URL4 = 'https://reddit.com'
+URL5 = 'http://www.e-prostor.gov.si'
 pdfURL = 'https://arxiv.org/pdf/1808.07042.pdf'
 
 
@@ -26,7 +30,7 @@ elif sys.platform.startswith('darwin'):
 # Instantiate headless chrome
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(executable_path='./chromedriver', options=chrome_options)
+driver = webdriver.Chrome(executable_path=platform_driver, options=chrome_options)
 
 
 class ContentType(Enum):
@@ -76,8 +80,23 @@ async def getSiteContent(url, contentType = ContentType.HTML):
         return text, [], []
 
 
+# urllib implementation does only basic functionality
+# It does not provide sitemap parsing
+def robotsparse(url):
+    parsedUrl = urlparse(url)
+    uo = str(urlopen(parsedUrl.scheme + "://" + parsedUrl.netloc + "/robots.txt").read())
+    rp.parse(uo)
+    sitemaps = re.findall("Sitemap:\s+([A-Za-z0-9\-\.\_\~\:\/\?\#\[\]\@\!\$\&\(\)\*\+\,\;\=\%]+)", uo)
+    locations = []
+    for sitemap in sitemaps:
+        sm = str(urlopen(sitemap).read())
+        locs = re.findall("<loc>([^<>]+)</loc>", sm)
+        locations.append(locs)
+    return rp, locations
+
 def seleniumGetContents(url):
     driver.get(url)
+    robotsparse(url)
     htmlContent = BeautifulSoup(driver.page_source, 'html5lib')
     # print(htmlContent)
     documents = []
@@ -85,6 +104,8 @@ def seleniumGetContents(url):
     parsedUrl = urlparse(url)
     rp.set_url(parsedUrl.scheme + "://" + parsedUrl.netloc + "/robots.txt")
     rp.read()
+    print(rp)
+    return
     if rp.can_fetch("*", url):
         for link in htmlContent.find_all('img'):
             current_link = link.get('src')
@@ -127,7 +148,7 @@ async def getBinaryFile(url):
 # print(doc)
 # print (images)
 
-html, doc, images = seleniumGetContents(URL3)
+html, doc, images = seleniumGetContents(URL5)
 print(html)
 print(doc)
 print(images)
